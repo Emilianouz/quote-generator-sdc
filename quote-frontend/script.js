d67bac45be36a6ddd511b4e1495f81e78ad26d5c
Emiliano Uruena <emilianouz@yahoo.es>
2026-01-09 12:05:49 +0000

    Update quote API endpoint to production URL
    from localhost to:
    https://emiliano-quote-generator-sdc-backend.hosting.codeyourfuture.io/

--- quote-frontend/script.js
+++ quote-frontend/script.js
@@ -4,7 +4,7 @@ const authorElement = document.getElementById("author");
 const button = document.getElementById("new-quote");
 
 async function fetchQuote() {
-  const response = await fetch("http://localhost:3000");
+  const response = await fetch("https://emiliano-quote-generator-sdc-backend.hosting.codeyourfuture.io");
   const data = await response.json();
   quoteElement.textContent = data.quote;
   authorElement.textContent = data.author
